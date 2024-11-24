9f9f10d0767cba6e2ad0b838432a8fc08d95502f
ron_shuster <118674717+shusteron@users.noreply.github.com>
2024-11-24 22:39:16 +0200

    fdgfgsfsdfsdfdsfsdfds

--- Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
+++ Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿public class DestroyOnTrigger2D : MonoBehaviour {
 
 private HealthManager healthManager;
@@ -88,4 +89,28 @@ public class DestroyOnTrigger2D : MonoBehaviour {
 //     }
 // }
 =======
+>>>>>>> parent of 6090528 (health destroyed with everything else)
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag.
+ */
+public class DestroyOnTrigger2D : MonoBehaviour {
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == triggeringTag && enabled) {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Update() {
+        /* Just to show the enabled checkbox in Editor */
+    }
+}
 >>>>>>> parent of 6090528 (health destroyed with everything else)
