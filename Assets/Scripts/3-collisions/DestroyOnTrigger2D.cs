415dcf7c1b99a834999bd3fe4340f0f0fe7ba1a6
ron_shuster <118674717+shusteron@users.noreply.github.com>
2024-11-24 22:08:19 +0200

    fail

--- Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
+++ Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -1,69 +1,65 @@
-﻿// using UnityEngine;
+﻿public class DestroyOnTrigger2D : MonoBehaviour {
 
-// public class DestroyOnTrigger2D : MonoBehaviour
-// {
-//     [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
-//     [SerializeField] string triggeringTag;
+private HealthManager healthManager;
 
-//     private HealthManager healthManager;  // Reference to the HealthManager
-
-//     private void Start()
-//     {
-//         // Find the HealthManager in the scene
-//         healthManager = FindObjectOfType<HealthManager>();
-//         if (healthManager == null)
-//         {
-//             Debug.LogError("HealthManager not found in the scene!");
-//         }
-//     }
-
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         if (other.tag == triggeringTag && enabled)
-//         {
-//             // Notify HealthManager to reduce health
-//             healthManager?.TakeDamage();
-
-//             // Destroy the enemy object
-//             Destroy(other.gameObject);
-//         }
-//     }
-// }
-
-
-
-using UnityEngine;
-
-public class DestroyOnTrigger2D : MonoBehaviour
-{
-    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
-    [SerializeField] string triggeringTag;
-
-    private HealthManager healthManager;  // Reference to the HealthManager
-
-    private void Start()
+    void Start()
     {
         // Find the HealthManager in the scene
         healthManager = FindObjectOfType<HealthManager>();
         if (healthManager == null)
         {
-            Debug.LogError("HealthManager not found in the scene!");
+            Debug.LogError("HealthManager not found in scene!");
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        // Only handle collisions with enemies and not UI or health point images
-        if (other.CompareTag("Enemy") && this.CompareTag("Player")) // If the player collides with an enemy
+        if (other.CompareTag("Enemy"))
         {
-            healthManager?.TakeDamage(); // Notify the HealthManager to reduce health
-            Destroy(other.gameObject);   // Destroy the enemy
-        }
-        else if (other.CompareTag("Laser") && this.CompareTag("Enemy")) // If the laser hits an enemy
-        {
-            Destroy(other.gameObject); // Destroy the laser
-            Destroy(this.gameObject);  // Destroy the enemy
+            // Player hit an enemy
+            healthManager.TakeDamage();
+            // Destroy the enemy
+            Destroy(other.gameObject);
         }
     }
+
+
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// /**
+//  * This component destroys its object whenever it triggers a 2D collider with the given tag.
+//  */
+// public class DestroyOnTrigger2D : MonoBehaviour {
+//     [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+//     [SerializeField] string triggeringTag;
+
+//     private void OnTriggerEnter2D(Collider2D other) {
+//         if (other.tag == triggeringTag && enabled) {
+//             Destroy(this.gameObject);
+//             Destroy(other.gameObject);
+//         }
+//     }
+
+//     private void Update() {
+//         /* Just to show the enabled checkbox in Editor */
+//     }
+// }
